f5f6c046c29e3fdfcbb62cea5d04e1986caf6cde
NikolyUrsegov <ursegovnikolaj@gmail.com>
2022-06-06 14:59:24 +0300

    add components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className={'header'}>
+                <img
+                    src='https://w7.pngwing.com/pngs/477/1005/png-transparent-user-computer-icons-logo-others-service-logo-computer-wallpaper.png'/>
+            </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img style={{width: '1000px'}}
+                     src={'https://foto.krasnoturinsk.me/cpg/albums/userpics/10073/P1020597.jpg'}/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New Post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
