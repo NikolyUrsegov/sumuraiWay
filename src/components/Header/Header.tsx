8c1f76f2d29c0f82350c494cd16200222434e723
NikolyUrsegov <ursegovnikolaj@gmail.com>
2022-06-28 15:42:34 +0300

    add MyPost, Post

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-            <header className={'header'}>
+            <header className={s.header}>
                 <img
                     src='https://w7.pngwing.com/pngs/477/1005/png-transparent-user-computer-icons-logo-others-service-logo-computer-wallpaper.png'/>
             </header>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import s from './Propfile.module.css'
+import MyPosts from "./MyPost/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={s.content}>
             <div>
                 <img style={{width: '1000px'}}
                      src={'https://foto.krasnoturinsk.me/cpg/albums/userpics/10073/P1020597.jpg'}/>
@@ -10,18 +12,7 @@ const Profile = () => {
             <div>
                 ava + description
             </div>
-            <div>
-                My posts
-            </div>
-            <div>
-                New Post
-            </div>
-            <div>
-                post 1
-            </div>
-            <div>
-                post 2
-            </div>
+            <MyPosts/>
         </div>
     );
 };
